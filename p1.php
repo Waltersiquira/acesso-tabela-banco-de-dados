05fb31b5a364be684851ccb11b3d79b67e07b15e
Waltersiquira <167370881+Waltersiquira@users.noreply.github.com>
2024-12-19 19:24:52 -0300

    Update p1.php

--- p1.php
+++ p1.php
@@ -7,6 +7,7 @@
 </head>
 <body>
     <?php 
+    require_once 'includes/funcoes.php';
     $user = $_GET['usuario'] ?? '';
     $senha = $_GET['senha'] ?? '';
     if ($user == 'Guilherme' and password_verify($senha, '$2y$10$FKCBxt.ne95pV3dsC16JMe63q.rRptAYjON02NCUsGcHd7Bq2adn2') == true or $user == 'Walter' and password_verify($senha, '$2y$10$FKCBxt.ne95pV3dsC16JMe63q.rRptAYjON02NCUsGcHd7Bq2adn2') == true or $user == 'João da Silva' and password_verify($senha, '$2y$10$/SfVXxJcV1Dgont1HeNCSOTL42blZKbXF9ZLxyVfEFbXWaNcmmsyW') == true){
